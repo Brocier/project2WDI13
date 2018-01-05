a85538b7664e241a1f1073bea276d171bb35e8c2
Brocier <jschoe04@gmail.com>
2018-01-05 14:38:29 -0500

    built out basic functionality in each of these controllers

---
+++ controllers/pets.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../db/models/Pets.js')
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+    res.render('users/index');
+});
+
+module.exports = router;

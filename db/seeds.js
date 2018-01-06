f7874926c4ddc85b823518854e155eee143f26e1
Brocier <jschoe04@gmail.com>
2018-01-06 13:44:44 -0500

    seperated for clarity

--- db/seeds.js
+++ db/seeds.js
@@ -24,45 +24,46 @@ mongoose.connection.on('error', (error) => {
 })
 
 //Wipe everything and add my seeds
-User.remove({}).then(() => {
-    const josh = new User({
-        username: 'jschoe04',
-        password: '12345', //thank you Spaceballs
-        firstname: 'Joshua',
-        //fill this out more later
-    })
+User.remove({})
+    .then(() => {
+        const josh = new User({
+            username: 'jschoe04',
+            password: '12345', //thank you Spaceballs
+            firstname: 'Joshua',
+            //fill this out more later
+        })
 
-    const tiberius = new Pet({
-        name: 'Tiberius',
-        age: 2,
-    })
-    const cuteCat = new Photo({
-        description: 'Laying on the couch',
-        photoUrl: 'https://i.imgur.com/v47JqKm.jpg'
-    })
+        const tiberius = new Pet({
+            name: 'Tiberius',
+            age: 2,
+        })
+        const cuteCat = new Photo({
+            description: 'Laying on the couch',
+            photoUrl: 'https://i.imgur.com/v47JqKm.jpg'
+        })
 
-    tiberius.photos.push(cuteCat)
+        tiberius.photos.push(cuteCat)
 
-    const murdock = new Pet({
-        name: 'Murdock',
-        age: 9,
-    })
-    const handsomeBoy = new Photo({
-        description: 'What a doggo',
-        photoUrl: 'https://imgur.com/bFFkBgF'
-    })
-    murdock.photos.push(handsomeBoy)
+        const murdock = new Pet({
+            name: 'Murdock',
+            age: 9,
+        })
+        const handsomeBoy = new Photo({
+            description: 'What a doggo',
+            photoUrl: 'https://imgur.com/bFFkBgF'
+        })
+        murdock.photos.push(handsomeBoy)
 
-    josh.pets.push(murdock, tiberius)
+        josh.pets.push(murdock, tiberius)
 
-    return josh.save()
-}).catch((error) => {
-    console.log('SEED NO WORKING')
-    console.log(error)
-}).then(() => {
-    mongoose.connection.close()
-    console.log(`
+        return josh.save()
+    }).catch((error) => {
+        console.log('SEED NO WORKING')
+        console.log(error)
+    }).then(() => {
+        mongoose.connection.close()
+        console.log(`
         Finished seeding database        
         Closed connection to MongoDB`)
-})
+    })
 

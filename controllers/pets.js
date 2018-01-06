9b283c96b526b2a63829bd13a537e5272d20a6c0
Brocier <jschoe04@gmail.com>
2018-01-06 15:30:46 -0500

    finally got the pets page to show up without stalling out

--- controllers/pets.js
+++ controllers/pets.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../db/models/Pets.js')
 
-/* GET home page. */
+const User = require('../db/models/User.js')
+
+
+/* GET pets home page. */
 router.get('/', (req, res) => {
-    res.render('pets/index');
+    const userId = req.params.userId
+    User.findById(userId)
+        .then((user) => {
+            res.render('pets/index', {
+                user,
+                //pets: user.pets,
+                //userName: user.firstname,
+                userId,
+                //petName: pet.name
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
 });
 
 module.exports = router;

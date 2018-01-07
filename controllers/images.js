97189df99c010ec57606e688cbabb16c699f9a6d
Brocier <jschoe04@gmail.com>
2018-01-06 20:33:29 -0500

    Added the show route for the images

--- controllers/images.js
+++ controllers/images.js
@@ -1,10 +1,81 @@
 const express = require('express');
-const router = express.Router();
-const User = require('../db/models/Images.js')
+const router = express.Router({ mergeParams: true });
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-    res.render('users/index');
-});
+const User = require('../db/models/User.js')
+
+
+router.get('/new', (req, res) => {
+    const userId = req.params.userId
+    const petId = req.params.petId
+
+    User.findById(userId)
+        .then((user) => {
+            const pet = user.pets.id(petId)
+
+            res.render('images/new', {
+                userId,
+                pet,
+            })
+        })
+})
+
+router.post('/', (req, res) => {
+    const userId = req.params.userId
+    const petId = req.params.petId
+
+    const newImage = req.body
+
+    User.findById(userId)
+        .then((user) => {
+            const pet = user.pets.id(petId)
+            pet.images.push(newImage)
+
+            return user.save()
+        })
+        .then(() => {
+            res.redirect(`/users/${userId}/pets/${petId}`)
+        })
+})
+
+router.get('/:imageId', (req, res) => {
+    const userId = req.params.userId
+    const petId = req.params.petId
+    const imageId = req.params.imageId
+
+    User.findById(userId)
+        .then((user) => {
+            const pet = user.pets.id(petId)
+            const image = pet.imagesToReturn.id(imageId)
+
+            res.render('images/show', {
+                userId,
+                pet,
+                image,
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
+
+router.get('/:imageId/delete', (req, res) => {
+    const userId = req.params.userId
+    const petId = req.params.petId
+    const imageId = req.params.imageId
+
+    User.findById(userId)
+        .then((user) => {
+            const pet = user.pets.id(petId)
+            pet.imagesToReturn.id(imageId).remove()
+
+            return user.save()
+        })
+        .then(() => {
+            res.redirect(`/users/${userId}/pets/${petId}`)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
 
 module.exports = router;

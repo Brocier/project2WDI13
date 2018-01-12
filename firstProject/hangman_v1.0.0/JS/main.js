f338f6ac3a27b876ba32644b630ba3e6ba920233
Brocier <jschoe04@gmail.com>
2018-01-12 10:36:20 -0500

    first project

---
+++ firstProject/hangman_v1.0.0/JS/main.js
@@ -0,0 +1,29 @@
+$(() => {
+    console.log(`let's guess!`)
+    const guessWord = `fhqwgads`
+    const letters = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z`]
+
+    //split up the answer into seperate letters
+    const guessSplit = guessWord.split("");
+    //map the split letters to divs with the class of 'hidden answer'
+    const guessFinal = guessSplit.map((letter) => {
+        return $('.answer-container').append(`<span class="hidden answer ${letter}">${letter}</span>`)
+    })
+    //make a click event that returns the letter clicked
+    const guessedLetter = $(`.letter`).on(`click`, (event) => {
+        const letterClicked = event.target.textContent;
+        console.log('letter', letterClicked);
+        if (guessSplit.indexOf(letterClicked) > -1) {
+            $(event.target).css('font-weight', 'bold')
+            $(event.target).off()
+            $(`.${letterClicked}`).removeClass('hidden')
+        }
+        else {
+            $(event.target).addClass('wrong');
+            $(event.target).off()
+            //remove one from a counter for wrong guesses
+
+        }
+        return letterClicked;
+    })
+});
---
+++ firstProject/hangman_v1.0.0/README.md
@@ -0,0 +1,20 @@
+# firstWDI13Project
+My first project for WDI13
+
+Github Link:
+https://github.com/Brocier/firstWDI13Project
+
+Trello Board:
+https://trello.com/b/k75g4M5t/wdi-project-1
+
+Figma board link:
+https://www.figma.com/file/ap1UnJWAukVNUUaHlDzpZC5c/WDI13-Project1-Wireframes
+
+
+BitBalloon link:
+http://wdi13projectonejschoenfeld.bitballoon.com/
+
+Spec sheet
+
+Hangman
+
